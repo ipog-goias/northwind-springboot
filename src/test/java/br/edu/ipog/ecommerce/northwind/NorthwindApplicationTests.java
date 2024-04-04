18676cebd4dc926595bcdccd5ca96bb627b80ed8
Bruno Urbano Rodrigues <brunourb@gmail.com>
2024-04-04 19:44:44 -0300

    chore: start project

---
+++ src/test/java/br/edu/ipog/ecommerce/northwind/NorthwindApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ipog.ecommerce.northwind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthwindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
